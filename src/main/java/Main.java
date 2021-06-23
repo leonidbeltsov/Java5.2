02fad735a42a5f2bb6da9bd0c8be0c57d1855027
Leonid Beltsov <leonidbeltsov@gmail.com>
2021-06-23 20:08:58 +0300

    edit Main.java + add screenshot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,13 @@ public class Main {
         boolean registered = true;
 
         long expected = 30;
+        // переменная expected - это ожидаемый результат расчета значения бонуса для выбранных значений переменных amount и registered
+
         long actual = service.calculate(amount, registered);
         boolean passed = expected == actual;
+        // т.к. мы рассматриваем Maven проект, который применяется для тестирования класса BonusService,
+        // то вводим новую переменную Passed для сравнения фактического результата расчета бонуса с ожидаемым,
+        // тем самым мы добьемся наглядности наших тестов.
 
         System.out.println(passed);
     }
